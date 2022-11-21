b57e40b0781f1540e2f1bb3cc6586fd9fb7cbc7d
michail <mihail.chertov@gmail.com>
2022-11-21 14:23:32 +0300

    Ex.05 ConIfElse/

---
+++ Ex.05_Cond.IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input name user: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "Masha")
+{
+   Console.WriteLine("Yp, this is Masha");
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(userName);
+}
